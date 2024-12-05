efa300d7da063d65e702d299e701fd38eaedac80
aldisti <alessandrodiste99@gmail.com>
2024-12-05 22:08:48 +0100

    upload solution

---
+++ day05/part1.cpp
@@ -0,0 +1,118 @@
+
+#include <unordered_set>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+
+using String = std::string;
+
+template <typename T>
+using Vector = std::vector<T>;
+template <typename T, typename D>
+using Map = std::map<T,D>;
+template <typename T>
+using Set = std::unordered_set<T>;
+
+using StringVector = Vector<String>;
+using IntVector = Vector<int>;
+
+String readFile(String filename) {
+    std::ifstream       file(filename);
+    std::stringstream   buffer;
+
+    if (!file.is_open())
+        throw std::runtime_error("Error: file not open!");
+
+    buffer << file.rdbuf();
+    return buffer.str();
+}
+
+StringVector split(String str, String separators, bool removeSeparator=true) {
+    StringVector    strs;
+    size_t          i = 0, j = 0;
+
+    while (i < str.size()) {
+        if (separators.find(str[i]) == String::npos) {
+            i++;
+            continue;
+        }
+        strs.push_back(str.substr(j, i - j + !removeSeparator));
+        j = ++i;
+    }
+    if (j < i)
+        strs.push_back(str.substr(j, i - j + !removeSeparator));
+    return strs;
+}
+
+Map<int, Set<int>> parseRules(StringVector lines, size_t &i) {
+    Map<int, Set<int>>  rules;
+    StringVector        splitted;
+    String              line;
+    size_t              j;
+
+    for (i = 0; i < lines.size(); i++) {
+        line = lines[i];
+        if (line.empty())
+            break;
+        splitted = split(line, "|");
+        j = std::stoi(splitted[0]);
+        if (rules.find(j) == rules.end())
+            rules[j] = Set<int>();
+        rules[j].insert(std::stoi(splitted[1]));
+    }
+    return rules;
+}
+
+Vector<IntVector>   parseUpdates(StringVector lines, size_t &i) {
+    Vector<IntVector>   updates;
+    IntVector           update;
+    StringVector        splitted;
+    String              line;
+
+    for (i++; i < lines.size(); i++) {
+        line = lines[i];
+        if (line.empty())
+            break;
+        splitted = split(line, ",");
+        update = IntVector();
+        for (String num : splitted)
+            update.push_back(std::stoi(num));
+        updates.push_back(update);
+    }
+    return updates;
+}
+
+// 4281
+int main(void) {
+    String              text = readFile("part1.txt");
+    Map<int, Set<int>>  rules;
+    Vector<IntVector>   updates;
+    size_t              i, j;
+    int                 sum;
+
+    StringVector lines = split(text, "\n"), splitted;
+
+    rules = parseRules(lines, i);
+    updates = parseUpdates(lines, i);
+
+    sum = 0;
+    for (const IntVector update : updates) {
+        for (i = 1; i < update.size(); i++) {
+            Set<int> ruleSet = rules[update[i]];
+            for (j = 0; j < i; j++)
+                if (ruleSet.find(update[j]) != ruleSet.end())
+                    break;
+            if (j != i)
+                break;
+        }
+        if (i != update.size())
+            continue;
+        sum += update[update.size() / 2];
+    }
+
+    std::cout << "The sum of middle numbers in the updates is " << sum << std::endl;
+    return (0);
+}
